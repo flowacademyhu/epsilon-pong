cc80709f62f22740cb98f4f5b2378dbf05ba2c3c
hajnalandor <hajnal.andor1993@gmail.com>
2019-02-24 17:58:25 +0100

    Tasi rewiew 0.1

--- ball.js
+++ ball.js
@@ -1,6 +1,7 @@
 let randomize = () => {
   return Math.random() < 0.5 ? 1 : -1;
 };
+
 let ball = {
   xPos: 50,
   yPos: 15,
@@ -9,44 +10,77 @@ let ball = {
   point: false,
   counter: 0,
   player1: 0,
-  player2: 0
+  player2: 0,
+  isDelay: false
 };
-const moveBall = (ball, arr) => {
+
+const leftSide = (ball, arr) => {
   if (ball.xPos === 1 && arr[ball.yPos][0] !== ' ') {
     ball.xDir = -1 * ball.xDir;
   } else if (ball.xPos === 1 && arr[ball.yPos][0] === ' ') {
-    ball.xPos = 50;
-    ball.yPos = 15;
-    ball.xDir = randomize();
-    ball.yDir = randomize();
-    ball.point = true;
-    ball.player2++;
-  }
-  if (ball.xPos === 98 && arr[ball.yPos][99] !== ' ') {
-    ball.xDir = -1 * ball.xDir;
-  } else if (ball.xPos === 98 && arr[ball.yPos][99] === ' ') {
-    ball.xPos = 50;
-    ball.yPos = 15;
-    ball.xDir = randomize();
-    ball.yDir = randomize();
-    ball.point = true;
-    ball.player1++;
-  }
-  if (ball.yPos === 1 || ball.yPos === 29) {
-    ball.yDir = -1 * ball.yDir;
-  }
-  if (ball.point === true && ball.counter < 30) {
-    ball.counter++;
-    // console.log('EZ SZÉP ÜTÉS VOLT!');
-    // console.log(`Az eredmeny ${ball.player1} : ${ball.player2}`);
-    return true;
-  } else if (ball.point === true && ball.counter === 30) {
-    ball.point = false;
-    ball.counter = 0;
-  } else {
-    ball.xPos += ball.xDir;
-    ball.yPos += ball.yDir;
+    pointEvent(ball, arr, ball.player2);
   }
   return ball;
 };
+
+const rightSide = (ball, arr) => {
+  if (ball.xPos === arr[0].length - 2 && arr[ball.yPos][arr[0].length - 1] !== ' ') {
+    ball.xDir = -1 * ball.xDir;
+  } else if (ball.xPos === arr[0].length - 2 && arr[ball.yPos][arr[0].length - 1] === ' ') {
+    pointEvent(ball, arr, ball.player1);
+  }
+  return ball;
+};
+
+const topBottom = (ball, arr) => {
+  if (ball.yPos === 1 || ball.yPos === arr.length - 1) {
+    ball.yDir = -1 * ball.yDir;
+  }
+  return ball;
+};
+
+const pointEvent = (ball, arr, player) => {
+  ball.xPos = 50;
+  ball.yPos = 15;
+  ball.xDir = randomize();
+  ball.yDir = randomize();
+  ball.point = true;
+  if (ball.player1 === player) {
+    ball.player1++;
+  } else {
+    ball.player2++;
+  }
+  return ball;
+};
+
+const delay = (ball) => {
+  if (ball.point === true && ball.counter < 30) {
+    ball.counter++;
+    ball.xDir = 0;
+    ball.yDir = 0;
+    ball.isDelay = true;
+  } else if (ball.point === true && ball.counter === 30) {
+    ball.point = false;
+    ball.counter = 0;
+    ball.xDir = randomize();
+    ball.yDir = randomize();
+    ball.isDelay = false;
+  }
+  return ball;
+};
+
+const moveBall = (ball, arr) => {
+  ball = leftSide(ball, arr);
+  ball = rightSide(ball, arr);
+  ball = topBottom(ball, arr);
+  ball = delay(ball);
+  if (ball.isDelay) {
+    return true;
+  }
+  ball.xPos += ball.xDir;
+  ball.yPos += ball.yDir;
+
+  return ball;
+};
+
 module.exports = { moveBall, ball };
